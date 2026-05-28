81f663290eb52774f14967367d0b9ab7ac58f137
Lucas Vinícius de L. Assis <lvlassis.2@gmail.com>
2026-05-27 21:35:53 -0300

    refactor: cria abstração de Character e carregamento da memória

---
+++ game/character.go
@@ -0,0 +1,72 @@
+package game
+
+import (
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/math32"
+	"github.com/g3n/engine/texture"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+)
+
+type characterData struct {
+	Name  string `yaml:"name"`
+	Image string `yaml:"image"`
+	MaxLife int `yaml:"life"`
+	Strenght int `yaml:"strenght"`
+	Speed int `yaml:"speed"`
+}
+
+type Character struct {
+	characterData
+	Sprite 		*graphic.Sprite
+	Life 		int
+}
+
+
+
+func LoadCharacter(path string) *Character {
+
+	// Lê o arquivo
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Erro ao ler arquivo %s: %s", path, err)
+	}
+
+	// Faz unmarshal do YAML
+	var charData characterData
+	err = yaml.Unmarshal(data, &charData)
+	if err != nil {
+		log.Fatalf("Erro ao parsear YAML de %s: %s", path, err)
+	}
+
+	// Carrega a imagem em uma textura
+	char_texture, err := texture.NewTexture2DFromImage(charData.Image)
+	if err != nil {
+		log.Fatalf("Falha ao carregar %s: %s", charData.Image, err)
+	}
+	
+	// Ajusta os parâmetros da textura
+	char_texture.SetMagFilter(gls.NEAREST)
+	char_texture.SetMinFilter(gls.NEAREST)
+
+	// Cria material com a textura
+	char_material := material.NewStandard(math32.NewColor("white"))
+	char_material.AddTexture(char_texture)
+	char_material.SetTransparent(true)
+
+	// Cria Sprite com Material
+	var proportion float32 = float32(char_texture.Height()) / float32(char_texture.Width())
+	char_sprite := graphic.NewSprite(1, proportion, char_material)
+
+	// Cria e retorna o Character usando os dados lidos
+	return &Character{
+		characterData: charData, 
+		Life: charData.MaxLife, 
+		Sprite: char_sprite,
+	}
+}
+
+
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"lvlassis/moon-world-turns/game"
+	"time"
+
 	"github.com/g3n/engine/app"
 	"github.com/g3n/engine/camera"
 	"github.com/g3n/engine/core"
@@ -15,32 +20,10 @@ import (
 	"github.com/g3n/engine/texture"
 	"github.com/g3n/engine/util/helper"
 	"github.com/g3n/engine/window"
-	"log"
-	"time"
 )
 
-func NewCharacter(id string, position int, image_path string) *graphic.Sprite {
-	char_texture, err := texture.NewTexture2DFromImage(image_path)
-	if err != nil {
-		log.Fatal("Failed to load assets/ness.png: ", err)
-	}
-	var proportion float32 = float32(char_texture.Height()) / float32(char_texture.Width())
-	
-	char_texture.SetMagFilter(gls.NEAREST)
-	char_texture.SetMinFilter(gls.NEAREST)
-	// Create material with the texture
-	char_material := material.NewStandard(math32.NewColor("white"))
-	char_material.AddTexture(char_texture)
-	char_material.SetTransparent(true)
-	// Create sprite using the proper Sprite type
-	char_sprite := graphic.NewSprite(1, proportion, char_material)
-
-	if position == 1 {
-		char_sprite.SetPosition(-1, proportion/2, 0)
-	} else if position == 2 {
-		char_sprite.SetPosition(1, proportion/2, 0)
-	}
-	return char_sprite
+func DesenhaStatus(char *game.Character) string {
+	return fmt.Sprintf("%s HP: %d / %d", char.Name, char.Life, char.MaxLife)
 }
 
 func DesenhaCena(scene *core.Node) {
@@ -129,20 +112,61 @@ func main() {
 
 	// Cria Char 1
 	// Load Ness sprite texture
-	char1 := NewCharacter("charmander", 1, "assets/charmander.png")
-	scene.Add(char1)
+	char1 := game.LoadCharacter("./data/charmander.yaml")
+	char1.Sprite.SetPosition(-1, 0.5, 0)
+	scene.Add(char1.Sprite)
 
-	char2 := NewCharacter("squirtle", 2, "assets/squirtle.png")
-	scene.Add(char2)
+	char2 := game.LoadCharacter("./data/squirtle.yaml")
+	char2.Sprite.SetPosition(1, 0.5, 0)
+	scene.Add(char2.Sprite)
 
 	DesenhaCena(scene)
+
+	// ------- UI ----------
+
+	// Indicador de vida
+	lifeText := gui.NewLabel("")
+	lifeText.SetPosition(10, 10)
+	lifeText.SetColor(math32.NewColor("white"))
+	text := fmt.Sprintf("%s HP: %d / %d | %s HP: %d / %d", char1.Name, char1.Life, char1.MaxLife, char2.Name, char2.Life, char2.MaxLife)
+	lifeText.SetText(text)	
+	scene.Add(lifeText)
+
+	// Create button in the bottom-right corner
+	button := gui.NewButton("Attack")
+	button.SetSize(120, 40)
+	button.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		log.Println("Attacking")
+		char2.Life -= char1.Strenght
+	})
+	scene.Add(button)
+
+	// Position button in the bottom-right corner (updated on resize)
+	positionButton := func() {
+		_, height := a.GetSize()
+		button.SetPosition(float32(10), float32(height-50))
+	}
+	positionButton()
+
+	// Update button position when window is resized
+	a.Subscribe(window.OnWindowSize, func(evname string, ev interface{}) {
+		positionButton()
+	})
+
 
 	// Set background color to gray
 	a.Gls().ClearColor(0.8, 0.8, 0.8, 1.0)
 
 	// Run the application
 	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
+		// Limpa a Tela
 		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+
+		// Atualiza o estado da UI
+		text := fmt.Sprintf("%s HP: %d / %d | %s HP: %d / %d", char1.Name, char1.Life, char1.MaxLife, char2.Name, char2.Life, char2.MaxLife)
+		lifeText.SetText(text)	
+
+		// Renderiza a cena
 		renderer.Render(scene, cam)
 	})
 }

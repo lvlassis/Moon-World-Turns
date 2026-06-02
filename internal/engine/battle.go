c9f87232bf14e5ac8bbdb5d7a841b6305e1c2017
Sevenings <lvlassis.2@gmail.com>
2026-06-01 23:28:04 -0300

    feat: inicio do sistema de Actions

--- internal/engine/battle.go
+++ internal/engine/battle.go
@@ -12,52 +12,13 @@ type Battle struct {
 	turn		int
 }
 
+
 func NewBattle(character1, character2 *Character, bus *events.Bus) *Battle {
 	b := &Battle{
 		character1: character1,
 		character2: character2,
 	}
-	bus.Subscribe(events.CharacterAction, b.OnAction)
 	return b
-}
-
-func (b *Battle) Start() {
-	// Lógica para iniciar a batalha entre character1 e character2
-	fmt.Printf("A batalha começou entre %s e %s!\n", b.character1.Name, b.character2.Name)
-
-	// Define quem começa com base na velocidade dos personagens
-	fastest := b.character1.Speed > b.character2.Speed
-	if fastest {
-		b.choosing = b.character1
-	} else {
-		b.choosing = b.character2
-	}
-
-	fmt.Printf("%s começa a batalha!\n", b.choosing.Name)
-	fmt.Printf("Aguardando a ação de %s...\n", b.choosing.Name)
-}
-
-func (b *Battle) OnAction(eventName string, eventData interface{}) {
-	data, ok := eventData.(events.CharacterActionData)
-	if !ok {
-		return
-	}
-
-	// Verifica se o personagem que realizou a ação é o que está escolhendo
-	if data.CharacterID != b.choosing.ID {
-		return
-	}
-
-	// Mostra a ação realizada
-	fmt.Printf("Character %s fez uma ação!\n", data.CharacterID)
-
-	// Passa o turno
-	b.turn++
-	if b.choosing == b.character1 {
-		b.choosing = b.character2
-	} else {
-		b.choosing = b.character1
-	}
 }
 
 // GetCharacter1 retorna o primeiro personagem da batalha
@@ -70,7 +31,41 @@ func (b *Battle) GetCharacter2() *Character {
 	return b.character2
 }
 
-func (b *Battle) Attack(attacker *Character, defender *Character) {
+
+// Inicia a batalha, definindo quem começa e aguardando as ações dos personagens
+func (b *Battle) Start() {
+	// Lógica para iniciar a batalha entre character1 e character2
+	fmt.Printf("A batalha começou entre %s e %s!\n", b.character1.Name, b.character2.Name)
+
+	// Define quem começa com base na velocidade dos personagens
+	b.choosing = b.FirstCharacter(b.character1, b.character2)
+
+	fmt.Printf("%s começa a batalha!\n", b.choosing.Name)
+	fmt.Printf("Aguardando a ação de %s...\n", b.choosing.Name)
+}
+
+
+// Define o personagem quem começa
+func (b *Battle) FirstCharacter(char1, char2 *Character) *Character {
+	if char1.Speed > char2.Speed {
+		return char1
+	} else {
+		return char2
+	}
+}
+
+
+func (b *Battle) DoAction(character *Character, action Action, target *Character) {
+	if character != b.choosing {
+		fmt.Printf("Não é a vez de %s agir!\n", character.Name)
+		return
+	}
+	character.Execute(action, target)
+	b.nextTurn()
+}
+
+
+func (b *Battle) attack(attacker *Character, defender *Character) {
 	if attacker != b.choosing {
 		fmt.Printf("Não é a vez de %s atacar!\n", attacker.Name)
 		return
